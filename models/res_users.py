7342ed1dfa36c3d5f5a0bfb3b17d39af9ffc99ec
Abdullah Yousuf <condou23@gmail.com>
2026-08-28 16:51:22 +0300

    fixed issue in portal users

--- models/res_users.py
+++ models/res_users.py
@@ -22,13 +22,14 @@ class ResUsers(models.Model):
         if employee:
             return employee
 
-        # 2. Fallback search by partner_id (work_contact_id or address_home_id)
+        # 2. Fallback search by partner_id (work_contact_id)
         if self.partner_id:
             employee = Employee.search([
                 ('active', '=', True),
-                '|',
                 ('work_contact_id', '=', self.partner_id.id),
-                ('address_home_id', '=', self.partner_id.id)
+                '|',
+                ('company_id', '=', False),
+                ('company_id', '=', self.company_id.id)
             ], limit=1)
             if employee:
                 return employee
